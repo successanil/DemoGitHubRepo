ef7a6cf7068c928b1b0255aabe32ee07617d32d8
Relsell <relsellglobal@gmail.com>
2017-06-24 06:22:40 +0530

    changes

--- GitOne/app/src/main/java/in/relsellglobal/gitone/MainActivity.java
+++ GitOne/app/src/main/java/in/relsellglobal/gitone/MainActivity.java
@@ -1,8 +1,14 @@
+/*
+ * Copyright (c) 2017. Relsell Global
+ */
+
 package in.relsellglobal.gitone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// changes to project
 public class MainActivity extends AppCompatActivity {
 
     @Override
